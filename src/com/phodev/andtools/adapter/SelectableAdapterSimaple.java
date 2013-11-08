1bedf679ea5cd5f50c97e271029a18c53a62b745
sky <shenkaige86@gmail.com>
2013-11-08 09:32:15 +0800

    完善demo

--- src/com/phodev/andtools/adapter/SelectableAdapterSimaple.java
+++ src/com/phodev/andtools/adapter/SelectableAdapterSimaple.java
@@ -1,16 +1,22 @@
 package com.phodev.andtools.adapter;
 
+import java.util.ArrayList;
+
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 
 public class SelectableAdapterSimaple extends
-		SelectableAdapter<Integer, Object> {
+		SelectableAdapter<Integer, String> {
+	private static final int POSITION_ID = 0xf213129;
 
-	@Override
-	public int getCount() {
-		// return super.getCount();
-		return 20;// test
+	public SelectableAdapterSimaple() {
+		mData = new ArrayList<String>();
+		for (int i = 0; i < 20; i++) {
+			mData.add("item" + i);
+		}
 	}
 
 	@Override
@@ -20,13 +26,28 @@ public class SelectableAdapterSimaple extends
 			cb = (CheckBox) convertView;
 		} else {
 			cb = new CheckBox(parent.getContext());
+			cb.setOnCheckedChangeListener(onCheckedChangeListener);
 		}
+		//
+		cb.setTag(POSITION_ID, position);
 		cb.setChecked(isCheckedData(getData(position)));
 		return cb;
 	}
 
+	private OnCheckedChangeListener onCheckedChangeListener = new OnCheckedChangeListener() {
+		@Override
+		public void onCheckedChanged(CompoundButton buttonView,
+				boolean isChecked) {
+			Object obj = buttonView.getTag(POSITION_ID);
+			if (obj instanceof Integer) {
+				int position = (Integer) obj;
+				setChecked(position, isChecked);
+			}
+		}
+	};
+
 	@Override
-	protected Integer getItemCheckRecordKey(Object t) {
+	protected Integer getItemCheckRecordKey(String t) {
 		return t.hashCode();
 	}
 
