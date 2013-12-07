94dfee6d27ceafb73b4e811597357d67924e04f5
sky <shenkaige86@gmail.com>
2013-12-08 03:58:32 +0800

    add html template prefix

--- src/com/phodev/andtools/common/CommonParam.java
+++ src/com/phodev/andtools/common/CommonParam.java
@@ -2,4 +2,5 @@ package com.phodev.andtools.common;
 
 public class CommonParam {
 	public static final boolean DEBUG = true;
+	public static final String PAGE_DRIVER_TEMPLATE_DIR = "file:///android_asset/page_driver/";
 }
