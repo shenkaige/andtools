5898cf683162cb0ecd0dbc47a86db9a969a5ccf6
skg <shenkaige86@gmail.com>
2016-06-30 11:19:23 +0800

    change base class to ImageView

--- src/com/phodev/andtools/widget/SquareImageView.java
+++ src/com/phodev/andtools/widget/SquareImageView.java
@@ -2,14 +2,14 @@ package com.phodev.andtools.widget;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 /**
  * 正方形ImageView
  * 
  * @author sky
  */
-public class SquareImageView extends FrameLayout {
+public class SquareImageView extends ImageView {
 
 	public SquareImageView(Context context, AttributeSet attrs) {
 		super(context, attrs);
